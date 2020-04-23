a08ae265035c6ec55e9dd5d5960dc7a5bf97122a
loldebyte <47870314+loldebyte@users.noreply.github.com>
2020-04-23 19:29:34 +0200

    added comments

--- java/gameoflife/Grid.java
+++ java/gameoflife/Grid.java
@@ -19,22 +19,22 @@ public class Grid {
     }
 
     private void generateRandomInitialState() {
-        this.cells = new Cell[sizeGrid][sizeGrid];
+        this.cells = new Cell[sizeGrid][sizeGrid];              // initialize cells array
         for (int i=0; i<sizeGrid; i++) {
             for (int j=0; j<sizeGrid; j++) {
-                this.cells[i][j] = new Cell(rd.nextBoolean());
+                this.cells[i][j] = new Cell(rd.nextBoolean());  // fills cells with random bools
             }
         }
     }
 
-    public int getnbAliveNeighbours(int i, int j) {
+    public int getnbAliveNeighbours(int i, int j) { 
         int result =0;
         for (int k=i-1; k<i+2; k++) {
             for (int l=j-1; l<j+2; l++) {
-                if (!(k==i && l==j)) {
-                    if (k>=0 && l>=0 && k<this.sizeGrid && l<this.sizeGrid) {
+                if (!(k==i && l==j)) {  // avoid counting current cell
+                    if (k>=0 && l>=0 && k<this.sizeGrid && l<this.sizeGrid) {   // avoid out of range indexes
                         if (this.cells[k][l].isAlive()) {
-                            result++;
+                            result++;   // if target cell is alive, increment the number of alive neighbours
                         }
                     }
                 }
@@ -44,30 +44,29 @@ public class Grid {
     }
 
     public void generateNextState() {
-        int[][] nbAliveNeighbours = new int[this.sizeGrid][this.sizeGrid];
+        int[][] nbAliveNeighbours = new int[this.sizeGrid][this.sizeGrid];  // initialze an array of the same size as cells
         for (int i=0; i<sizeGrid; i++) {
             for (int j=0; j<sizeGrid; j++) {
-                nbAliveNeighbours[i][j] = getnbAliveNeighbours(i, j);
+                nbAliveNeighbours[i][j] = getnbAliveNeighbours(i, j); // nbAliveNeighbours[i][j] = number of alive neighbours of cell
             }
         }
-        for (int i=0; i<sizeGrid; i++) { // getnbAliveNeighbours
+        for (int i=0; i<sizeGrid; i++) {
             for (int j=0; j<sizeGrid; j++) {
-                this.cells[i][j].setIsAlive(Cell.processState(this.cells[i][j].isAlive(), nbAliveNeighbours[i][j]));
+                this.cells[i][j].setIsAlive(Cell.processState(this.cells[i][j].isAlive(), nbAliveNeighbours[i][j])); // processes cells
             }
         }
     }
 
     public String toString() {
-        // return this.cells.toString();
-        String result = "";
+        String result = ""; // initialize our return with an empty string to concat with/to
         for (int i=0; i<this.sizeGrid; i++) {
             for (int j=0; j<this.sizeGrid; j++) {
-                result = result.concat(this.cells[i][j].toString());
+                result = result.concat(this.cells[i][j].toString());    // concat current cell's value
                 if (j!=this.sizeGrid-1)
-                    result = result.concat(" ");
+                    result = result.concat(" ");                        // concat additional space UNLESS we're at the last cell of the row...
             }
             if (i != this.sizeGrid-1)
-                result = result.concat("\n");
+                result = result.concat("\n");                           // unless we're at the last row, concat a carriage return
         }
         return result;
     }
