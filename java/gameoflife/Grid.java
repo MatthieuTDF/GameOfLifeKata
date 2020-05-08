8f7c55ce3914c969a57791a9aaa2fc8af541b9a5
Eduard Serban <serbanedi.fr@gmail.com>
2020-05-08 15:08:07 +0200

    Finit

--- java/gameoflife/Grid.java
+++ java/gameoflife/Grid.java
@@ -20,14 +20,54 @@ public class Grid {
 
     private void generateRandomInitialState() {
         // TODO
+        this.cells = new Cell[sizeGrid][sizeGrid];
+        for (int i = 0; i<sizeGrid;  i++){
+            for (int j = 0; j<sizeGrid;  j++){
+                this.cells[i][j] = new Cell(rd.nextBoolean());
+            }
+        }
+
+    }
+
+    public int getAliveNeighbours(int posx, int posy){
+        int res = 0;
+        for (int k = posx - 1; k < posx +2 ; k++){
+            for (int l = posy-1; l < posy + 2; l++){
+                if (k != posx && l != posy){
+                    if (k >= 0 && l >= 0 && k < this.sizeGrid && l < this.sizeGrid){
+                        if (this.cells[k][l].isAlive()){
+                            res ++;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
     }
 
     public void generateNextState() {
         // TODO
+        for (int i = 0; i< this.sizeGrid;  i++) {
+            for (int j = 0; j < this.sizeGrid; j++) {
+                int neighbours = getAliveNeighbours(i, j);
+                boolean alive = this.cells[i][j].isAlive();
+                this.cells[i][j].setIsAlive(Cell.processState(alive, neighbours));
+            }
+        }
     }
 
     public String toString() {
         // TODO
-        return "";
+        String output = "";
+        for (int i = 0; i < this.sizeGrid;  i++) {
+            for (int j = 0; j < this.sizeGrid; j++) {
+                output = output.concat(this.cells[i][j].toString());
+                if (j == this.sizeGrid - 1){
+                    output = output.concat("\n");
+                }
+            }
+
+        }
+        return output;
     }
 }
